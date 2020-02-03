af22d87a09b798c25f27cb3f807310746242d5da
nguyenphuochai1998 <haito1998@gmail.com>
2020-02-03 14:38:08 +0700

    create project
    create project

---
+++ android/app/src/main/kotlin/com/nguyenphuochai/flutter_app_learn_eng/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.nguyenphuochai.flutter_app_learn_eng
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
